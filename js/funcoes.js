e5d01d6dd653d8ed87f6eff81b59b1ce9716dd37
giovannimiranda <63267655+giovannimiranda@users.noreply.github.com>
2020-06-15 19:39:50 -0300

    Update funcoes.js

--- js/funcoes.js
+++ js/funcoes.js
@@ -8,15 +8,15 @@ function calcular() {
 
     if (imc < 17)
         formulario.situacao.value = "Você se encontra muito abaixo do peso.";
-    else if (imc >= 17 && imc < 18,49)
+    else if (imc >= 17 && imc < 18.49)
         formulario.situacao.value = "Você se encontra abaixo do peso.";
-    else if (imc >= 18,5 && imc < 24,99)
+    else if (imc >= 18,5 && imc < 24.99)
         formulario.situacao.value = "Você se encontra no peso normal.";
-    else if (imc >= 25 && imc < 29,99)
+    else if (imc >= 25 && imc < 29.99)
         formulario.situacao.value = "Você se encontra acima do peso.";
-    else if (imc >= 30 && imc < 34,99)
+    else if (imc >= 30 && imc < 34.99)
         formulario.situacao.value = "Você se encontra em obesidade I.";
-    else if (imc >= 35 && imc < 39,99)
+    else if (imc >= 35 && imc < 39.99)
         formulario.situacao.value = "Você se encontra em obesidade II (Severa).";
     else if (imc > 40)
         formulario.situacao.value = "Você se encontra em obesidade III (Mórbida).";
